bd4690087b48ed42efa11cad282c344d7c9c6f3d
Nicolas Lima <nichollaslimma734@gmail.com>
2023-06-07 14:19:25 -0300

    adicionado validação para recebimento de numeros negativos

--- advinhacao.c
+++ advinhacao.c
@@ -17,18 +17,28 @@ int main(){
         printf("Qual eh o seu chute? ");
         scanf("%d", &chute);
         printf("seu chute foi %d", chute);
+
+        if(chute < 0){
+            printf("\nValor invalido, Por favor forneca um numero positivo\n");
+            i--;
+            continue;
+        }
+
         int acertou = (chute == numero_secreto);
+        int maior = (chute > numero_secreto);
+
         if(acertou){
             printf("\nParabens! Voce acertou!\n");
             break;
-        }else{
-            int maior = (chute > numero_secreto);
-            if(maior){
-                printf("\nSeu chute foi maior do que o numero secreto\n");
-            } else{
-                printf("\nSeu chute foi menor do que o numero secreto\n");
-            }
         }
+
+        else if(maior){
+            printf("\nSeu chute foi maior do que o numero secreto\n");
+        } 
+        else{
+            printf("\nSeu chute foi menor do que o numero secreto\n");
+        }
+
     }
 
     printf("\nFim de jogo!\n");
